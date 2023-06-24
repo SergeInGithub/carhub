eb634340523e28ed6bc04d4eec85a2c0dfdb316b
SergeInGithub <nyiriserge@gmail.com>
2023-06-24 13:00:58 +0200

    fix(bugs): fix bugs and issues

--- utils/index.ts
+++ utils/index.ts
@@ -33,7 +33,7 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 };
 
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-  const url = new URL('https://cdn.imahin.studio/getimage');
+  const url = new URL('https://cdn.imagin.studio/getimage');
 
   const { make, year, model } = car;
 
